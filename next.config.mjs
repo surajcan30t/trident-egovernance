0246413876cc57fb182aae2076f028c15334fb52
Suraj Kumar Sahoo <elitecracker25@gmail.com>
2024-12-31 11:35:14 +0530

    disable browser source map

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,18 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'assets.aceternity.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'ais.tact.ac.in',
+      },
     ],
+  },
+  productionBrowserSourceMaps: false, // Disable source maps
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.devtool = false; // Disable source maps in dev for client
+    }
+    return config;
   },
 };
 
