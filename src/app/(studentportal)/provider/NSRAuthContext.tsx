7c0d34f824e7357e2dea3b038b5a5214fd87c9f0
Suraj Kumar Sahoo <elitecracker25@gmail.com>
2024-12-31 12:41:22 +0530

    remain state for NSR logged in users

---
+++ src/app/(studentportal)/provider/NSRAuthContext.tsx
@@ -0,0 +1,24 @@
+'use client'
+import React, { createContext, useContext, useState } from 'react';
+
+interface AuthContextType {
+  isLoggedIn: boolean;
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AuthContext = createContext<AuthContextType>({
+  isLoggedIn: false, // Default state
+  setIsLoggedIn: () => {},
+});
+
+export const NSRAuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
