2083c4b45d9b30bb0545d4228c0b7ab989ad9c3c
Suraj Kumar Sahoo <elitecracker25@gmail.com>
2025-01-03 12:10:16 +0530

    add query params for searching functionality

--- src/app/(app)/(feecollection)/feecollection/feecollectiondetails/page.tsx
+++ src/app/(app)/(feecollection)/feecollection/feecollectiondetails/page.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import FinancialYearSelector from '@/app/(app)/(feecollection)/components/financial-year-selecter-button';
 import FeeCollectionTable from '@/app/(app)/(feecollection)/components/fee-collection-table';
 
-const page = () => {
+const page = (props: any) => {
+  const { searchParams } = props;
+  const today = new Date();
+  const defaultQuery = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`
+  const query:string = searchParams.get('search') || defaultQuery;
   return (
     <>
       <main className={'flex flex-col items-center border'}>
         <div>
 
         </div>
-        <div className={''}>
-          <div className={'flex justify-end m-2'}>
+        <div className=''>
+          <div className='flex justify-end m-2'>
             <FinancialYearSelector />
           </div>
-          <FeeCollectionTable />
+          { query && <FeeCollectionTable query={query} /> }
         </div>
       </main>
     </>
--- src/app/(app)/(feecollection)/feecollection/studentfeecollection/page.tsx
+++ src/app/(app)/(feecollection)/feecollection/studentfeecollection/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { RedirectStudentFeeCollectionForm } from '@/app/(app)/(feecollection)/components/RegdNumForm';
-import DailyCollection from '@/app/(app)/(feecollection)/components/DailyCollection';
-import FeeCollectionDashboardSingleStudent
-  from '@/app/(app)/(feecollection)/components/fee-collection-dashboard-single-student';
+import FeeCollectionDashboardSingleStudent from '@/app/(app)/(feecollection)/components/fee-collection-dashboard-single-student';
 
-const page = () => {
+interface Props {
+  searchParams: {
+    registrationNo: string;
+  };
+}
+
+const page = (props: Props) => {
+  const { registrationNo } = props.searchParams;
   return (
     <>
       <div className='w-full h-full flex flex-col p-2 items-center gap-4'>
