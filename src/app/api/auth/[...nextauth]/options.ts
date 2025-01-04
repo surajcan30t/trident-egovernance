1feb696a460c7209e0c6c2070cc9e16a08d4aa27
Suraj Kumar Sahoo <elitecracker25@gmail.com>
2025-01-04 20:38:39 +0530

    change session strategy to jwt and update maxage & updateage

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,7 +1,6 @@
 import { NextAuthOptions } from 'next-auth';
 import AzureADProvider from 'next-auth/providers/azure-ad';
-import { JWT, getToken } from 'next-auth/jwt';
-import { redirectLogic } from '@/lib/redirectlogic';
+import { JWT } from 'next-auth/jwt';
 
 const AZURE_AD_CLIENT_ID = process.env.AZURE_AD_CLIENT_ID!;
 const AZURE_AD_CLIENT_SECRET = process.env.AZURE_AD_CLIENT_SECRET!;
@@ -23,10 +22,11 @@ export const authOptions: NextAuthOptions = {
       issuer: AZURE_AD_ISSUER,
     }),
   ],
-  // session: {
-  //   strategy: 'jwt',
-  //   maxAge: 60 * 60, // 1 hour
-  // },
+  session: {
+    strategy: 'jwt',
+    maxAge: 60 * 60, // Session expires after 1 hour of inactivity
+    updateAge: 0, // Update session with each request to keep it active while user is on page
+  },
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async jwt({ token, account }): Promise<JWT> {
