6810a923f4ac8732253ffa04bd6228501f401a14
Suraj Kumar Sahoo <elitecracker25@gmail.com>
2025-01-21 12:33:46 +0530

    remove data from localstorage after form submit

--- src/app/(app)/(accounts)/accounts/generatefeestructure/page.tsx
+++ src/app/(app)/(accounts)/accounts/generatefeestructure/page.tsx
@@ -4,15 +4,14 @@ import Unauthorized from '@/components/Unauthorized';
 import GenerateFeeStructureMultistepForm from '../../components/GenerateFeeStructureMultistepForm';
 
 const page = async () => {
-  // const { session, role, token } = await authValidator();
-  // if (!session || !token) {
-  //   return <Unauthorized />;
-  // }
+  const { session, role, token } = await authValidator();
+  if (!session || !token) {
+    return <Unauthorized />;
+  }
 
-  // if (role !== 'ACCOUNTS') {
-  //   return <Unauthorized />;
-  // }
-  const token = '1234567890'
+  if (role !== 'ACCOUNTS') {
+    return <Unauthorized />;
+  }
   return (
     <div className='flex flex-col items-center justify-center gap-y-4'>
       <GenerateFeeStructureMultistepForm token={token}/>
