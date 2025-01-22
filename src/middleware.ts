6764db103cf8fbbc0709fcea4adb78463be309f5
Suraj Kumar Sahoo <elitecracker25@gmail.com>
2025-01-22 11:33:52 +0530

    update matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -63,121 +63,12 @@ export default withAuth(
   },
 );
 
-// Do not uncomment this. This is not required
-// export default withAuth(
-//   async function middleware(req) {
-//     console.log('correct url: ', req.nextUrl.pathname);
-//     const token = req.nextauth.token as Token; // Type assertion to Token interface
-//     console.log('Token:', token);
-//
-//     const userRole = token?.userType?.jobTitle;
-//
-//     // If the user is logged in and requests the home page ('/')
-//     if (userRole && req.nextUrl.pathname === '/') {
-//       const redirectionRoute = serverRoutesArr[0]; // Redirect to the zeroth index route based on the user role
-//       console.log('Redirecting logged-in user to:', redirectionRoute);
-//
-//       // Prevent access to home page and redirect logged-in users to their role-based page
-//       return NextResponse.redirect(new URL(redirectionRoute, req.url));
-//     }
-//
-//     // Role-based redirection if visiting any other page
-//     if (
-//       userRole &&
-//       serverRoutesArr.length > 0 &&
-//       req.nextUrl.pathname.startsWith('/')
-//     ) {
-//       const redirectionRoute = serverRoutesArr[0]; // Redirect to the zeroth index route
-//       console.log('Redirecting to:', redirectionRoute);
-//
-//       // Prevent redirect loop by checking if the user is already on the correct route
-//       if (req.nextUrl.pathname !== redirectionRoute) {
-//         return NextResponse.redirect(new URL(redirectionRoute, req.url));
-//       }
-//     }
-//
-//     // If no valid token or role, allow access to public pages like the home page ('/')
-//     return NextResponse.next();
-//   },
-//   {
-//     callbacks: {
-//       authorized: ({ token }) => {
-//         return !!token; // Allow access if token exists
-//       },
-//     },
-//   },
-// );
-
-// interface UserType {
-//   jobTitle: string;
-// }
-
-// interface Token {
-//   userType: UserType; // The role of the user, such as "RECEPTIONIST", "DOCTOR", etc.
-// }
-
-// export async function middleware(request: NextRequest) {
-//   const isPublicRoutes = ["/", "/studentportal", "/studentportal/:path*"];
-//   const pathname = request.nextUrl.pathname;
-
-//   // Check if the current path matches any public route
-//   const isPublic = isPublicRoutes.some(route => {
-//     const regex = new RegExp(`^${route.replace(':path*', '.*')}$`);
-//     return regex.testpage(pathname);
-//   });
-
-//   // Fetch the token
-//   const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET }) as Token | null;
-
-//   console.log('token in middleware: ', token);
-
-//   // If there's no token and the route is not public, redirect to the login page
-//   if (!token && !isPublic) {
-//     return NextResponse.redirect(new URL("/", request.url));
-//   }
-
-//   // Check the role and redirect based on the role
-//   if (token) {
-//     switch (token.userType.jobTitle) {
-//       case "job_student":
-//         if (!request.nextUrl.pathname.startsWith("/dashboard")) {
-//           return NextResponse.redirect(new URL("/dashboard", request.url));
-//         }
-//         break;
-//       case "job_office":
-//         if (!request.nextUrl.pathname.startsWith("/feecollectionhome")) {
-//           return NextResponse.redirect(new URL("/feecollectionhome", request.url));
-//         }
-//         break;
-//       case "NURSE":
-//         if (!request.nextUrl.pathname.startsWith("/vitals")) {
-//           return NextResponse.redirect(new URL("/vitals", request.url));
-//         }
-//         break;
-//       case "PATHOLOGIST":
-//         if (!request.nextUrl.pathname.startsWith("/image")) {
-//           return NextResponse.redirect(new URL("/image", request.url));
-//         }
-//         break;
-//       default:
-//         return NextResponse.redirect(new URL("/", request.url));
-//     }
-//   }
-
-//   // Allow the request to continue
-//   return NextResponse.next();
-// }
-
 export const config = {
   matcher: [
-    // '/((?!api|_next/static|_next/image|.*\\.png$|favicon.ico|login|studentportal).*)',
-    '/student/:path*',
-    '/newstudentregistration',
-    '/office/:path*',
-    // '/feecollection/dashboard',
-    // '/feecollection/feecollectiondetails',
-    // '/feecollection/studentfeecollection',
-    // '/feecollection/otherfeecollection',
-    // '/feecollection/duestatusreport',
+    '/((?!api|_next/static|_next/image|.*\\.png$|favicon.ico|login|studentportal|verifymr|accounts/studentfeecollection/mr).*)(.+)',
+    // '/student/:path*',
+    // '/newstudentregistration',
+    // '/office/:path*',
+    // '/accounts/:path*',
   ],
 };
