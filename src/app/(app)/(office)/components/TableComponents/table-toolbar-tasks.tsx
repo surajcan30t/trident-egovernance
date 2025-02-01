cce62b86a805ee930f61695b537d9228e960cf2f
Suraj Kumar Sahoo <elitecracker25@gmail.com>
2025-02-01 18:29:53 +0530

    office table actions

---
+++ src/app/(app)/(office)/components/TableComponents/table-toolbar-tasks.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { Table } from "@tanstack/react-table"
+
+import { PromoteStudentsDialog } from "../session-initiation/PromoteStudentsDialog"
+
+interface SessionWiseStudentData {
+  regdNo: string,
+  studentName: string
+}
+interface TableToolbarTasksProps {
+  table: Table<any>
+}
+
+
+export function TableToolbarTasks({
+  table,
+}: TableToolbarTasksProps) {
+  return (
+    <div className="flex items-center gap-2">
+      {table.getFilteredSelectedRowModel().rows.length > 0 ? (
+        <PromoteStudentsDialog
+          studentData={table
+            .getFilteredSelectedRowModel()
+            .rows.map((row) => row.original)}
+          onSuccess={() => table.toggleAllRowsSelected(false)}
+        />
+      ) : null}
+    </div>
+  )
+}
